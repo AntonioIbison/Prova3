00072b235c0f60286bc1553a0ecaacdccb6e4b84
Ibison <112359840+AntonioIbison@users.noreply.github.com>
2023-07-09 10:48:22 -0300

    Add files via upload

---
+++ Prova3/src/execucao/Main.java
@@ -0,0 +1,15 @@
+package execucao;
+import controle.Controle;
+import janelas.Janela;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/*sugestao para persistencia salvar as cartas da rodada:
+		o jogador da vez nao tem cartaPalpite pq ele n joga entao ao salvar a DELE use o att CARTA,
+		ja para os outros utilize a cartaPalpite */
+
+		Controle controle= new Controle();
+
+	}
+}
